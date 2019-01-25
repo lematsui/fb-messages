15e5f710fdab74c362062cdce5a42e70b2933904
Ariel LeRoy <arielmatsuileroy@gmail.com>
2019-01-25 02:15:44 +0000

    Brain.js is too slow, will be swithing to Python

--- train.js
+++ train.js
@@ -3,16 +3,30 @@ const fs = require('fs');
 
 const network = new brain.recurrent.LSTM();
 
-network.train([
-  { input: "hello", output: "hi!"},
-  { input: "bye", output: "goodbye!"},
-  { input: "goodbye", output: "bye"},
-  { input: "hi", output: "hi!"},
-  { input: "hi!", output: "hello!"},
-  { input: "bye!", output: "goodbye!"},
-  { input: "hello!", output: "hi!"},
-]);
+const text = fs.readFileSync('./convo.txt', 'utf8').replace(/(\d{2}\s\w{3}\s\d{4},\s\d{2}:\d{2})|(\w{3}\s\d{2}:\d{2})/g, '');
+const lines = text.split('\n');
+const filtered = lines.filter(function (el) {
+  return el != '';
+});
 
+const userOne = [];
+const userTwo = [];
+
+for (let i = 0; i < filtered.length; i++) {
+  if (i % 2 === 0) {
+    userOne.push(filtered[i]);
+  } else {
+    userTwo.push(filtered[i]);
+  };
+};
+
+const data = [];
+for (let i = 0; i < userOne.length; i++) {
+  data.push({ input: userOne[i], output: userTwo[i]})
+};
+
+console.log("Training, please stand by!")
+network.train(data);
 console.log('Done training!');
 
 const networkJSON = JSON.stringify(network.toJSON());
